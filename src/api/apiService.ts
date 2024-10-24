24aa0907a8d701953cecb44078facd4ee72e2d83
altinaqx <altinaspahija@hotmail.com>
2024-10-24 21:37:45 +0200

    axios client and requests created

---
+++ src/api/apiService.ts
@@ -0,0 +1,34 @@
+import axiosClient from './axiosClient';
+
+const apiService = {
+  getAll: async <T>(url: string): Promise<T[]> => {
+    const response = await axiosClient.get(url);
+    return response.data;
+  },
+
+  getById: async <T>(url: string, id: string): Promise<T> => {
+    const response = await axiosClient.get(`${url}/${id}`);
+    return response.data;
+  },
+
+  getByQuery: async <T>(url: string, queryParams: any): Promise<T[]> => {
+    const response = await axiosClient.get(url, { params: queryParams });
+    return response.data;
+  },
+
+  post: async <T>(url: string, data: T): Promise<T> => {
+    const response = await axiosClient.post(url, data);
+    return response.data;
+  },
+
+  put: async <T>(url: string, id: string, data: T): Promise<T> => {
+    const response = await axiosClient.put(`${url}/${id}`, data);
+    return response.data;
+  },
+
+  delete: async (url: string, id: string): Promise<void> => {
+    await axiosClient.delete(`${url}/${id}`);
+  },
+};
+
+export default apiService;
