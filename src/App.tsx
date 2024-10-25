7983eaf9066ae8b953d6396430ec7a099b7fc0ff
altinaqx <altinaspahija@hotmail.com>
2024-10-25 22:42:30 +0200

    quick fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import AppRouter from "./AppRouter";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Jobs from "./pages/Jobs";
+import JobsDetails from "./components/jobs/JobDetails";
 
 export default function App() {
   return (
-      <AppRouter />
+    <Router>
+    <Routes>
+      <Route path="/" element={<Jobs />} />
+      <Route path="/:id" element={<JobsDetails />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  </Router>
   );
 }
